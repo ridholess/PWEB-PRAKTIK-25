825925f1a71a6ee1ebea8c8994544a0a9639ea6c
ridholess <newridho1298@gmail.com>
2025-12-10 13:14:52 +0700

    feat: week10

--- myproject/app.py
+++ myproject/app.py
@@ -1,13 +1,15 @@
 from flask import Flask
-from flask_mysqldb import MySQL
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config["MYSQL_HOST"] = "localhost"
-app.config['MYSQL_PORT'] = 3306
-app.config["MYSQL_USER"] = "root"
-app.config["MYSQL_PASSWORD"] = ""
-app.config["MYSQL_DB"] = "db_books"
-mysql = MySQL(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+pymysql://root:@localhost:3306/db_books'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_RECORD_QUERIES"] = True
+
+db = SQLAlchemy(app)
+from myproject.models.books import Books
+migrate = Migrate(app, db)
 
 from myproject.controllers.books import get_books, create_book, edit_book, delete_book as del_book
 
--- myproject/controllers/books.py
+++ myproject/controllers/books.py
@@ -1,5 +1,6 @@
 from flask import request, Response
-from myproject.models.books import get_all_books_asc, get_all_books_desc, insert_book, edit_book_by_id, delete_book_by_id
+from myproject.app import db
+from myproject.models.books import Books
 from myproject.utils.res_wrapper import success_response, error_response
 
 
@@ -7,18 +8,24 @@ def get_books() -> str:
     """
     Mendapatkan buku-buku yang ada
     """
-    if request.args.get('order') == 'asc':
-        books = get_all_books_asc()
-    else:
-        books = get_all_books_desc()
-    # Melakukan mapping data
-    books = map(lambda book: {
-        "id": book[0],
-        "title": book[1],
-        "author": book[2],
-        "created_at": book[3]
-    }, books)
-    books = list(books)
+    # Mendapatkan seluruh data
+    books = Books.query.all()
+    # Cara 1
+    temp = []
+    for book in books:
+        temp.append({
+            "id": book.id,
+            "title": book.title,
+            "author": book.author,
+            "created_at": book.created_at
+        })
+    # Cara 2
+    books = list(map(lambda book: {
+        "id": book.id,
+        "title": book.title,
+        "author": book.author,
+        "created_at": book.created_at
+    }, books))
     return success_response(data=books)
 
 
@@ -30,7 +37,11 @@ def edit_book(book_id: int) -> str | Response:
         if request.json.get("title") and request.json.get("author"):
             new_title = request.json['title']
             new_author = request.json['author']
-            edit_book_by_id(book_id, new_title, new_author)
+            Books.query.filter_by(id=book_id).update({
+                "title": new_title,
+                "author": new_author
+            })
+            db.session.commit()
             return success_response(msg="Buku berhasil diubah", res_code=200)
         return error_response("Data tidak lengkap", res_code=400)
     return error_response("Method not allowed", res_code=405)
@@ -41,7 +52,8 @@ def delete_book(book_id: int) -> Response:
     Menghapus buku berdasarkan id
     """
     if request.method == "DELETE":
-        delete_book_by_id(book_id)
+        Books.query.filter_by(id=book_id).delete()
+        db.session.commit()
         return success_response("Buku berhasil dihapus", res_code=200)
     return error_response("Method not allowed", res_code=405)
 
@@ -54,7 +66,16 @@ def create_book() -> str | Response:
         if request.json.get("title") and request.json.get("author"):
             title = request.json['title']
             author = request.json['author']
-            insert_book(title, author)
-            return success_response("Data berhasil ditambah", res_code=201)
+            new_book = Books(title=title, author=author)
+            db.session.add(new_book)
+            db.session.commit()
+            return success_response("Data berhasil ditambah",
+                                    data={
+                                        "id": new_book.id,
+                                        "title": new_book.title,
+                                        "author": new_book.author,
+                                        "created_at": new_book.created_at
+                                    },
+                                    res_code=201)
         return error_response("Data tidak lengkap", res_code=400)
     return error_response("Method not allowed", res_code=405)
